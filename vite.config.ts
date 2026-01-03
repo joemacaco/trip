d303af0a39ef92664fca750bc87253ab8a594470
Joe Ma <joe23y@gmail.com>
2026-01-03 12:38:39 +0000

    replace path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // 如果您的 GitHub 網址是 https://<USERNAME>.github.io/<REPO>/
   // 請將 base 改為 '/<REPO>/'。如果是自定義網域則保持 '/'
-  base: './', 
+  base: '/trip/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
